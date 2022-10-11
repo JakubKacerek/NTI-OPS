856526dd0f0c9227d7f156a9cb466d974529f526
Jakub Káčerek <jakub.kacerek@tul.cz>
2022-10-11 11:32:48 +0200

    const

--- DISCO-F746NG_blink_leda/main.cpp
+++ DISCO-F746NG_blink_leda/main.cpp
@@ -3,25 +3,27 @@
 
 Serial pc(USBTX, USBRX);
 DigitalOut myled(LED1);
+const int SHORT = 0.2;
+const int LONG = 0.6;
 
 void blinkLed(float one, float two, float three, float four, float five) {
     if (one > 0){
         myled = 1;wait(one);
     }
     if (two > 0){
-        myled = 0; wait(0.2);
+        myled = 0; wait(SHORT);
         myled = 1; wait(two);
     }
     if (three > 0){
-        myled = 0; wait(0.2);
+        myled = 0; wait(SHORT);
         myled = 1; wait (three);
     }
     if (four > 0){
-        myled = 0; wait(0.2);
+        myled = 0; wait(SHORT);
         myled = 1; wait(four);
     }
     if (five > 0){
-        myled = 0; wait(0.2);
+        myled = 0; wait(SHORT);
         myled = 1; wait(five);
     }
     myled = 0; wait(2);
@@ -29,13 +31,13 @@ void blinkLed(float one, float two, float three, float four, float five) {
 
 void blinkLed(char morse) {
     switch (morse) {
-        case 'A' : blinkLed(0.2, 0.6,0,0,0); break;
-        case 'D' : blinkLed(0.6, 0.2,0.2,0,0); break;
-        case 'M' : blinkLed(0.6, 0.6,0,0,0); break;
-        case 'J' : blinkLed(0.2, 0.6,0.6,0.6,0); break;
-        case 'K' : blinkLed(0.6, 0.2,0.6,0,0); break;
-        case 'U' : blinkLed(0.2, 0.2,0.6,0,0); break;
-        case 'B' : blinkLed(0.6, 0.2,0.2,0.2,0); break;
+        case 'A' : blinkLed(SHORT, LONG,0,0,0); break;
+        case 'D' : blinkLed(LONG, SHORT,SHORT,0,0); break;
+        case 'M' : blinkLed(LONG, LONG,0,0,0); break;
+        case 'J' : blinkLed(SHORT, LONG,LONG,LONG,0); break;
+        case 'K' : blinkLed(LONG, SHORT,LONG,0,0); break;
+        case 'U' : blinkLed(SHORT, SHORT,LONG,0,0); break;
+        case 'B' : blinkLed(LONG, SHORT,SHORT,SHORT,0); break;
         default : blinkLed(0,0,0,0,0); break;
     }
 }
