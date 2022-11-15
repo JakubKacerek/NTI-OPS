fea70e45cc35c96fd320a3ff913a68b7f894edb0
Jakub Káčerek <jakub.kacerek@tul.cz>
2022-11-15 11:22:21 +0100

    update-už to jede

--- main.cpp
+++ main.cpp
@@ -1,37 +1,50 @@
+#include "./functions.h"
 #include "mbed.h"
 #include "stm32746g_discovery_lcd.h"
+#include <iostream>
+#include <random>
 
-Thread t1, t2;
+Thread thread1;
+Thread thread2;
+Thread thread3;
 
+void displayOnDisplay(int xPosition, int yPosition, int number) {
+  BSP_LCD_DisplayStringAt(xPosition, yPosition, (uint8_t *)"ADAM JAKUB" + number, CENTER_MODE);
+  ThisThread::sleep_for(THREAD_DURATION);
+  BSP_LCD_Clear(LCD_COLOR_BLACK);
 
-
-void neco(int x, int y, int time){
-        BSP_LCD_SetBackColor(LCD_COLOR_WHITE);
-        BSP_LCD_SetTextColor(LCD_COLOR_DARKBLUE);
-        BSP_LCD_DisplayStringAt(x, y, (uint8_t *)"Já jsem 1", CENTER_MODE);
-        ThisThread::sleep_for(time);
-}
-void runThread1(){
-    while(true){
-        neco(2, 0, 1000);
-    }
 }
 
-void runThread2(){
-    while(true){
-        neco(4, 4, 2000);
-    }
+void startThread1() {
+  while (true) {
+    displayOnDisplay(0, 50, 5);
+  }
 }
-int main()
-{
-    BSP_LCD_Init();
-    BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, LCD_FB_START_ADDRESS);
-    BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
-    BSP_LCD_SetFont(&LCD_DEFAULT_FONT);
-    BSP_LCD_Clear(LCD_COLOR_WHITE);
-    t1.start(runThread1);
-    t2.start(runThread2);
-    while (true) {
-        neco(3, -3, 500);
-    }
+
+void startThread2() {
+  while (true) {
+    displayOnDisplay(0, 100, 1);
+  }
+}
+
+void startThread3() {
+  while (true) {
+    displayOnDisplay(0, 150, 7);
+  }
+}
+int main() {
+  BSP_LCD_Init();
+  BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, LCD_FB_START_ADDRESS);
+  BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
+
+  BSP_LCD_Clear(LCD_COLOR_BLACK);
+  BSP_LCD_SetFont(&LCD_DEFAULT_FONT);
+  BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
+  BSP_LCD_SetTextColor(LCD_COLOR_MAGENTA);
+
+  thread1.start(startThread1);
+  thread2.start(startThread2);
+  thread3.start(startThread3);
+  while (true) {
+  }
 }
